ac8a8bec53124e83c1e9d458a4813d9dd2a426f4
rohit5454-choudhary <rohitchoudhary1906@gmail.com>
2026-02-02 21:15:27 +0530

    variable java

--- hello/youtube/one.java
+++ hello/youtube/one.java
@@ -1,3 +1,5 @@
+package hello.youtube;
+
 public class one {
 
     public static void main(String [] args){
---
+++ hello/youtube/variable.java
@@ -0,0 +1,33 @@
+ public class variable{
+
+    public static void main(String [] args){
+// variable
+
+int a = 23;
+ 
+int b = 32;
+
+int sum = a+b;
+
+System.out.println(sum);
+ 
+
+int diff = b-a;
+System.out.println(diff);
+
+
+int mul = a*b;
+ 
+System.out.println(mul);
+
+int ans = a*b / a-b;
+System.out.println(ans);
+    }
+
+     
+}
+
+
+ 
+    
+
