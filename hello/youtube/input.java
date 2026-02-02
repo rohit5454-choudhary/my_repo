9a569e16dd975f1c2be937565437fea9f1b02227
rohit5454-choudhary <rohitchoudhary1906@gmail.com>
2026-02-02 21:27:03 +0530

    input java

---
+++ hello/youtube/input.java
@@ -0,0 +1,22 @@
+package hello.youtube;
+
+
+import java.util.*;
+
+
+
+public class input {
+
+    public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+
+String name = sc.nextLine();
+//nextInt()
+//nextFloat()
+System.out.println(name);
+
+
+    }
+    
+}
